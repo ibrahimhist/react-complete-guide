cf417088035cd6f33bd78d3e3e662cf70bf44105
ibrahimhist <hist.ibrahim@gmail.com>
2020-12-08 10:03:47 +0300

    aux

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import './Person.css';
 import classes from './Person.css';
 import styled from 'styled-components';
+
+import Aux from '../../../hoc/Auxiliary';
 
 const StyledDiv = styled.div`
   width: 60%;
@@ -16,22 +18,52 @@ const StyledDiv = styled.div`
   }
 `;
 
-const person = (props) => {
+class Person extends Component {
   // const style = {
   //   '@media (min-width:500px)': {
   //     width: '450px',
   //   },
   // };
 
-  return (
-    <StyledDiv>
-      <p onClick={props.click}>
-        I am a {props.name} I am years old {props.age}
-      </p>
-      <p>{props.children}</p>
-      <input type='text' onChange={props.changed} value={props.name} />
-    </StyledDiv>
-  );
-};
+  render() {
+    return (
+      <StyledDiv>
+        <p onClick={this.props.click}>
+          I am a {this.props.name} I am years old {this.props.age}
+        </p>
+        <p>{this.props.children}</p>
+        <input
+          type='text'
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
+      </StyledDiv>
 
-export default person;
+      // <Aux>
+      //   <p onClick={this.props.click}>
+      //     I am a {this.props.name} I am years old {this.props.age}
+      //   </p>
+      //   <p>{this.props.children}</p>
+      //   <input
+      //     type='text'
+      //     onChange={this.props.changed}
+      //     value={this.props.name}
+      //   />
+      // </Aux>
+
+      //  <React.Fragment>
+      //   <p onClick={this.props.click}>
+      //     I am a {this.props.name} I am years old {this.props.age}
+      //   </p>
+      //   <p>{this.props.children}</p>
+      //   <input
+      //     type='text'
+      //     onChange={this.props.changed}
+      //     value={this.props.name}
+      //   />
+      // </React.Fragment>
+    );
+  }
+}
+
+export default Person;
